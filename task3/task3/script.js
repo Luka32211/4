48e545184ebb56ea8fd8e51d91397c6cebaf864e
Luka32211 <117283950+Luka32211@users.noreply.github.com>
2022-12-14 20:40:29 +0300

    test4

---
+++ task3/task3/script.js
@@ -0,0 +1,10 @@
+const trafficLightEl = document.querySelector('#trafficLight');
+
+function makeGreen() {
+    trafficLightEl.style.background = ('green');
+    trafficLightEl.removeEventListener('click', makeGreen);
+    trafficLightEl.addEventListener('click', makeYellow);
+}
+
+trafficLightEl.addEventListener('click', makeGreen);
+
